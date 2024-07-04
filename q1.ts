0459e2018522f6cf25369bd7eb38c42411874e8a
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-07-04 15:14:11 +0700

    660612137 กชภัส พันธ์ทอง

--- q1.ts
+++ q1.ts
@@ -1,15 +1,23 @@
 // define interface for Hero and Shop objects
-interface Hero {
-  /* Your code here */
+interface hero {
+  items: string[];
+  gold: number;
 }
 
-interface Shop {
-  /* Your code here */
+interface shop {
+  items: string[];
+  price: number;
 }
 
 // assign interface/type to the function definition properly
 function buyItem(hero, shop) {
-  /* Your code here */
+  if(hero.gold >= shop.price ){
+    hero.items.push(shop.items)
+    hero.gold -= shop.price;
+    return hero
+  }else {
+    return hero
+  }
 }
 
 //Test cases : assign proper type/interface to all objects
